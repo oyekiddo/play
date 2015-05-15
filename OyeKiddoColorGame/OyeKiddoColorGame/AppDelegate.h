e58d0f580abde3e0757bd50d93e629ad3bbf4a26
GregOmi <greg.omi@gmail.com>
2015-05-15 15:10:18 -0700

    deleting OpenEars frameworks as per request

---
+++ OyeKiddoColorGame/OyeKiddoColorGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OyeKiddoColorGame
+//
+//  Created by Gregory Omi on 5/14/15.
+//  Copyright (c) 2015 GregoryOmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OyeKiddoColorGame/OyeKiddoColorGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OyeKiddoColorGame
+//
+//  Created by Gregory Omi on 5/14/15.
+//  Copyright (c) 2015 GregoryOmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
