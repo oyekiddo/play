de6849b4acb5d998558663f913b4c4ceadccdd48
Halle Winkler <winkler@politepix.com>
2015-06-01 09:43:18 +0200

    Removal of non-redistributable software

--- OyeKiddo/Framework/RapidEarsDemo.framework/RapidEarsDocumentation/HTML/annotated.js
+++
@@ -1,5 +0,0 @@
-var annotated =
-[
-    [ "OEEventsObserver+RapidEars", "category_o_e_events_observer_07_rapid_ears_08.html", "category_o_e_events_observer_07_rapid_ears_08" ],
-    [ "OEPocketsphinxController+RapidEars", "category_o_e_pocketsphinx_controller_07_rapid_ears_08.html", "category_o_e_pocketsphinx_controller_07_rapid_ears_08" ]
-];
--- OyeKiddo/Framework/RejectoDemo.framework/RejectoDocumentation/HTML/search/classes_6f.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['oelanguagemodelgenerator_28rejecto_29',['OELanguageModelGenerator(Rejecto)',['../category_o_e_language_model_generator_07_rejecto_08.html',1,'']]]
-];
